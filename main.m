709d74d1fde7ab54fdf66161b4c32d7f1effab36
Arshia Goshtasbi <arshia.goshtasbi@gmail.com>
2025-11-10 20:14:21 +0330

    💫 IDK Really what's happening.

--- main.m
+++ main.m
@@ -1,8 +1,8 @@
 % Parameters
-num_nodes = 10;
+num_nodes = 50;
 center_range = [-10, 10];
 
 % Generate centers evenly distributed across the range
 centers = linspace(center_range(1), center_range(2), num_nodes);
 
-W = linspace(-1, 1, num_nodes);
+W = linspace(-0.1, 0.1, num_nodes);
